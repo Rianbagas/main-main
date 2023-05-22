3fc1decbcb8de604f2eb6ec16c2d6aa20d766252
Rianbagas <bagasrian82@gmail.com>
2023-05-21 20:19:16 -0700

    commit satu

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>my blok</title>
+</head>
+<body>
+	<table border="1" width ="100">
+	<tr><td colspan="2"><center><h1>header</td></h1></center></tr>
+	<tr>
+	<td colspan="2"><center>
+		<a href="index.php">home</a>
+		<a href="about.php">about</a>
+		<a href="blog.php">blog</a>
+		<a href="contact.php">contact</a>
+		</tr>
+		<tr style="height: 250px;">
+			<td valign="top" width="80%">content</td>
+			<td valign="top" width="20%">reight menu</td>
+		</tr>
+		<tr><td colspan="2"><center><h6>footer</h6></center></td></tr>
+	</center></td>
+
+	</table>
+
+</body>
+</html>
